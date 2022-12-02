ab744c26129991d06efe88f3cbd030cb2386ce57
jonathanmlgy <110006780+jonathanmlgy@users.noreply.github.com>
2022-12-03 01:55:03 +0800

    CreatplayRound function

--- javascript.js
+++ javascript.js
@@ -1,9 +1,27 @@
+let userChoice = prompt("Choose: Rock, Paper, or Scissors;")
 
 function getComputerChoice() {
-
-let choices = ['Rock', 'Paper', 'Scissors'];
+let choices = ['rock', 'paper', 'scissors'];
 let choice = choices[Math.floor(Math.random() * choices.length)];
 return choice;
 }
 
-console.log(getComputerChoice());
+
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        return "You win!";
+    } else {
+        return "You loss!"
+    }
+}
+
+const playerSelection = userChoice;
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
